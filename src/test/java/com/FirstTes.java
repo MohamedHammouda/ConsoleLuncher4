920ea5f6a07843bdd671a6949d17ee847d383baa
mohamed <med_hammouda@yahoo.fr>
2022-01-07 21:49:44 +0100

    first commit

---
+++ src/test/java/com/FirstTes.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTes {
+	@Test
+	void test1() {
+		//fail("Not yet implemented");
+	}
+	@Test
+	void test2() {
+		//fail("Not yet implemented");
+	}
+
+}
